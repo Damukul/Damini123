ce0205ce900e0f5bdf9ee528ecb4cc8541d0a4a0
Damini Kulkarni <kdamini17@gmail.com>
2023-11-23 23:41:14 +0530

    changes

---
+++ HotelManagement/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './CommonScreen/landing/landing.component';
+
+const routes: Routes = [
+  {path:"", component: LandingComponent},
+  {path:"Admin", loadChildren:()=>import('./admin/admin.module').then(mod=>mod.AdminModule)},
+  {path:"Owner", loadChildren:()=>import('./owner/owner.module').then(mod=>mod.OwnerModule)},
+  {path:"User", loadChildren:()=>import('./user/user.module').then(mod=>mod.UserModule)}
+]; 
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
